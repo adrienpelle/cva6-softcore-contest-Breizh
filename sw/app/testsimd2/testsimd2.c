9f22fe82b08983209cf8a0a59c3bdb18a57d8ff6
adrienpelle <pelleadrien94@gmail.com>
2024-03-03 22:41:38 +0100

    intrisic __rv_add8() test

--- sw/app/testsimd2/testsimd2.c
+++ sw/app/testsimd2/testsimd2.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
 #include "uart.h"
+#include "rvp_intrinsic.h"
 
 int main(void)
 {
 
     // Inline assembly for the add8 operation
     int32_t elements[2] = {0xaa0200ff, 0xaa0300ff};
+    //intrisic type 
+    uintXLEN_t i_elements[2] = {0xaa0200ff, 0xaa0300ff};
 
     // Load 32-bit elements from the array
     int32_t element1 = elements[0];
@@ -13,7 +16,6 @@ int main(void)
 
     // Perform add8 operation on the loaded 32-bit elements
     int32_t result;
-
     asm volatile (
         // Perform add8 operation
         "add8 %[result], %[element1], %[element2];"
@@ -29,10 +31,12 @@ int main(void)
     int8_t octet2 = (result >> 16) & 0xFF;
     int8_t octet3 = (result >> 8) & 0xFF;
     int8_t octet4 = result & 0xFF;
-    
-    char buffer[55];
+    //Intrisic function call 
+    uintXLEN_t i_result = __rv_add8(i_elements[0], i_elements[1]);
+    char buffer[70];
     //snprintf(buffer, 55, "Y[7:0] = %u,Y[15:8] = %u, Y[23:16] = %u, Y[31:24] = %u", octet1, octet2, octet3, octet4);
-    snprintf(buffer, 55, "Y = %d", result);
+    //snprintf(buffer, 70, "asmY = %d", result);
+    snprintf(buffer, 70, "intrisicY = %d", i_result);
 
      UART_init(&g_uart_0,
              UART_115200_BAUD,
@@ -44,4 +48,3 @@ int main(void)
 
     return 0;
 }
-
